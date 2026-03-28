36597827a1947114e70785088628e311ddd89fab
Diptesh Shahi Thakuri <dipteshshahi@hotmail.com>
2026-03-28 15:06:16 -0400

    Reset homepage scroll on reload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Research from "@/components/Research";
 import Footer from "@/components/Footer";
+import HomeScrollReset from "@/components/HomeScrollReset";
 
 export default function Home() {
   return (
     <main className="w-full">
+      <HomeScrollReset />
+
       <div id="hero" className="mx-auto max-w-7xl px-8 pb-24 pt-32">
         <Hero />
       </div>
